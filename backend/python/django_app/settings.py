ef376758338266060eb13518c2f9f17c69b6461f
Aastha Kumari Tarco <tarcoaastha@gmail.com>
2026-04-04 00:04:16 +0530

    feat: implement dynamic product rendering with CSS animations and category tags

--- backend/python/django_app/settings.py
+++ backend/python/django_app/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "django_app.apps.DjangoAppConfig",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -132,3 +134,4 @@ mongoengine.connect(
     password='example',
     authentication_source='admin'
 )
+CORS_ALLOW_ALL_ORIGINS = True
